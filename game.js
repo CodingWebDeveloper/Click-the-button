ad35b1c864110e2d8644e9cd3b2724979ecc2f16
Miroslav Uzunov <miroslav.uzunov23@gmail.com>
2021-05-30 22:41:57 +0300

    commit

---
+++ game.js
@@ -0,0 +1,43 @@
+const buttonHolder = document.querySelector('#btn');
+const progressBar = document.querySelector('#progressBarFull');
+const container = document.querySelector('.container');
+const modal = document.querySelector('.modal');
+const buttonStartAgain = document.querySelector('#btn-start-again');
+const transparentbg = document.querySelector('.transparent-bg');
+let currentProgress = 0;
+let decreaseProgress = 8;
+let time = 0;
+
+startGame = () =>{
+    currentProgress = 0;
+    modal.style.visibility = `hidden`;
+        progressBar.style.width = `${currentProgress}%`;
+        progressBar.style.transition = `width 1350ms`;
+    defaultStyle();
+}
+
+
+buttonHolder.addEventListener('click', e=>{
+    currentProgress += 10;
+    progressBar.style.width = `${currentProgress}%`;
+    progressBar.style.transition = `width 350ms`;
+
+    if(currentProgress == 100)
+    {
+        modal.style.visibility = `visible`;
+        transparentbg.style.opacity = `0.3`;
+    }
+
+    buttonStartAgain.addEventListener('click', e=>{
+        startGame();
+        
+    })
+    
+})
+
+   
+
+defaultStyle = () => {
+    progressBar.style.transition = `width 450ms`;
+    transparentbg.style.opacity = `1`;
+}
